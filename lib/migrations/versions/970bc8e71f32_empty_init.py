e6dee3a282ac2d3601ecfbe0767e242977d01ac9
Parker Parham <parkerparham@gmail.com>
2023-04-06 18:11:59 -0400

    Done with lab

---
+++ lib/migrations/versions/970bc8e71f32_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 970bc8e71f32
+Revises: 
+Create Date: 2023-04-06 18:02:17.700372
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '970bc8e71f32'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
